86ab4b39546c9198cf7a978e72f49ce2251ecb60
Abrar <m.abrarali94@gmail.com>
2023-02-20 20:34:08 +0500

    Authentication Completed

--- app/controllers/users_controller.rb
+++
@@ -1,5 +0,0 @@
-class UsersController < ApplicationController
-    def user_params
-        params.require(:user).permit(:role, :email, :password, :password_confirmation, :remember_me)
-      end
-end
